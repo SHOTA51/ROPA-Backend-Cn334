a1d59748330f33fbfc50c0924abe2b13409b05e7
markalovv <markalov8@gmail.com>
2026-04-23 04:31:58 +0700

    .

--- config/prisma.js
+++ config/prisma.js
@@ -1,17 +1,5 @@
-const { PrismaClient } = require('@prisma/client');
-const mysql = require('mysql2')
+const { PrismaClient } = require('@prisma/client')
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-module.exports = { prisma, connection };
+module.exports = prisma
